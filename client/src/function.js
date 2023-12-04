99b57963aefa38fdb84ad68fa683e40846aa60ee
avshalomram <126399746+avshalomram@users.noreply.github.com>
2023-12-04 11:45:53 +0200

    times

---
+++ client/src/function.js
@@ -0,0 +1,3 @@
+const date = require('date-and-time');
+
+console.log(date.format(new Date(), 'YYYY-MM-DD HH-mm'));
--- client/src/pages/home/home.jsx
+++ client/src/pages/home/home.jsx
@@ -1,11 +1,36 @@
-import React from "react";  
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import {DateTime} from "luxon";
+async function fetchData(setHebrewDate) {
+    try {
+        const currentDate = DateTime.now().toISODate();
+        const {data: {times}} = await axios.get(`https://www.hebcal.com/zmanim?cfg=json&geonameid=295514&date=${currentDate}`)
+        const gs = DateTime.now() > DateTime.fromISO(times.sunset) && "on"
+        const { data } = await axios.get(`https://www.hebcal.com/converter?cfg=json&date=${currentDate}&g2h=1&strict=1&gs=${gs}`);
+        setHebrewDate(data.hebrew)
+
+    } catch (error) {
+        setTimeout(() => {
+            fetchData(setHebrewDate)
+        }, 500000);
+    }
+}
 
 export default function Home() {
-    return  <>
-    <main> 
-        <div className="info"></div>
-        <div className="prayTimes"></div>
-    </main>
-        
-    </>
-}   
+    const [hebrewDate, setHebrewDate] = useState("");
+
+    useEffect(() => {
+       fetchData(setHebrewDate)
+    }, []);
+
+    return (
+        <>
+            <main>
+                <div className="info"></div>
+                <div className="prayTimes">
+                    {hebrewDate}
+                </div>
+            </main>
+        </>
+    );
+}
